36c57aa7d80a72274c0cc958163240be0b1bf0bc
ramsy0dev <ramsy0dev@gmail.com>
2024-02-01 21:25:40 +0000

    Added mpv to deps

--- install.sh
+++ install.sh
@@ -1,36 +1,41 @@
 #!/usr/bin/bash
 
+# Constants
+FONT_NAME="Hack"
+
+# Colors
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # Install yay
-echo " [ + ] Installing yay..."
+echo " ${GREEN}[ + ]${NC} Installing yay..."
 
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 # Install wayland, hyprland
-echo " [ + ] Installing hyprland-git..."
+echo " ${GREEN}[ + ]${NC} Installing hyprland-git..."
 
 yay -Syyu
 yay -S hyprland-git
 
 # Install deps
-echo " [ + ] Installing the following deps: nemo, git, python, python-pip, neovim, dunst, htop, kitty, neofetch, qt6ct, rofi, eza, alacritty..."
+echo " ${GREEN}[ + ]${NC} Installing the following deps: nemo, git, python, python-pip, neovim, dunst, htop, kitty, neofetch, qt6ct, rofi, eza, alacritty, curl, zip, unzip, grim, slurp, qt6-wayland, mpv ..."
 
-yay -S nemo git python python-pip neovim waybar dunst htop kitty neofetch qt6ct rofi eza alacritty curl zip unzip grim slurp qt6-wayland
+yay -S nemo git python python-pip neovim waybar dunst htop kitty neofetch qt6ct rofi eza alacritty curl zip unzip grim slurp qt6-wayland mpv
 
 # Installing the Hack nerd font
-echo " [ + ] Installing the Hack Nerd font..."
+echo " ${GREEN}[ + ]${NC} Installing the Hack Nerd font..."
 
-font_name="Hack"
-
-echo " [ + ] Font url: https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
-curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
-echo " [ + ] creating fonts folder: ${HOME}/.fonts"
+echo " ${GREEN}[ + ]${NC} Font url: https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$FONT_NAME.zip"
+curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$FONT_NAME.zip"
+echo " ${GREEN}[ + ]${NC} creating fonts folder: ${HOME}/.fonts"
 mkdir -p  "$HOME/.fonts"
-echo " [ + ] unzip the $font_name.zip"
-unzip "$font_name.zip" -d "$HOME/.fonts/$font_name/" && rm "./$font_name.zip"
+echo " ${GREEN}[ + ]${NC} unzip the $FONT_NAME.zip"
+unzip "$FONT_NAME.zip" -d "$HOME/.fonts/$FONT_NAME/" && rm "./$FONT_NAME.zip"
 fc-cache -fv
 
 # Copying config files to $HOME/.config
-echo " [ + ] Copying config files to $HOME/.config ..."
+echo " ${GREEN}[ + ]${NC} Copying config files to $HOME/.config ..."
 
 cp ./configs/* $HOME/.config/ -r
 
